4ae081419f5ded662b89e4092a1a58c5976abd9c
pabloren <delasota00@gmail.com>
2024-04-09 17:54:32 +0100

    End of section 13.

---
+++ client-app/src/app/stores/userStore.ts
@@ -0,0 +1,67 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+import { Activity } from '../models/activity';
+import agent from '../api/agent';
+import { v4 as uuid } from 'uuid';
+import { format } from 'date-fns';
+import { User, UserFormValues } from '../models/user';
+import { store } from './store';
+import { router } from '../router/Routes';
+
+export default class UserStore {
+  user: User | null = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  get isLoggedIn() {
+    return !!this.user;
+  }
+
+  login = async (creds: UserFormValues) => {
+    try {
+      const user = await agent.Account.login(creds);
+      store.commonStore.setToken(user.token);
+      runInAction(() => {
+        this.user = user;
+      });
+      router.navigate('/activities');
+      store.modalStore.closeModal();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  register = async (creds: UserFormValues) => {
+    try {
+      const user = await agent.Account.register(creds);
+      store.commonStore.setToken(user.token);
+      runInAction(() => {
+        this.user = user;
+      });
+      router.navigate('/activities');
+      store.modalStore.closeModal();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  logout = () => {
+    store.commonStore.setToken(null);
+    this.user = null;
+    router.navigate('/');
+  };
+
+  getUser = async () => {
+    try {
+      const user = await agent.Account.current();
+      runInAction(() => {
+        this.user = user;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
